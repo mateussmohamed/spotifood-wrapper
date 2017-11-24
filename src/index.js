67dab616e81ab3892b8e9671bb7ad90a55347ca9
Mateus Santana <mateuss.dev@gmail.com>
2017-11-24 12:29:52 -0200

    Exports all methods in index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import {
+  search,
+  searchArtists,
+  searchAlbums,
+  searchTracks,
+  searchPlaylists,
+} from '../src/search';
+
+import {
+  getAlbum,
+  getAlbums,
+  getAlbumTracks,
+} from '../src/album';
+
+module.exports = {
+  search,
+  searchArtists,
+  searchAlbums,
+  searchTracks,
+  searchPlaylists,
+  getAlbum,
+  getAlbums,
+  getAlbumTracks,
+};

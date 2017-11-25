7299348ad2ae22106cc5af8036c88d8612e2b8ae
Mateus Santana <mateuss.dev@gmail.com>
2017-11-24 22:03:37 -0200

    Refactoring album api

--- src/album.js
+++ src/album.js
@@ -1,17 +1,7 @@
-import { toJSON, wrapperFetch } from './utils';
-import { API_URL } from './config';
-
-export const getAlbum = (id) => {
-  const url = `${API_URL}/albums/${id}`;
-  return wrapperFetch(url).then(toJSON);
-};
-
-export const getAlbums = (ids) => {
-  const url = `${API_URL}/albums?ids=${ids}`;
-  return wrapperFetch(url).then(toJSON);
-};
-
-export const getAlbumTracks = (id) => {
-  const url = `${API_URL}/albums/${id}/tracks`;
-  return wrapperFetch(url).then(toJSON);
-};
+export default function album() {
+  return {
+    getAlbum: id => this.request(`${this.apiURL}/albums/${id}`),
+    getAlbums: ids => this.request(`${this.apiURL}/albums?ids=${ids}`),
+    getTracks: id => this.request(`${this.apiURL}/albums/${id}/tracks`),
+  };
+}

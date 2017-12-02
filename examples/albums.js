e1e2a2c4b3016614f5d7aa0f9c9d424d2a8f9e88
Mateus Santana <mateuss.dev@gmail.com>
2017-12-02 12:33:30 -0200

    Update examples

--- examples/albums.js
+++ examples/albums.js
@@ -3,9 +3,15 @@ import SpotifyWrapper from '../src/index';
 global.fetch = require('node-fetch');
 
 const spotifyWrapper = new SpotifyWrapper({
-  token: 'BQB3dooxjB7sn6lDto2XTcMtILN6QeowUn8MDmxmtr_PxCi3fNaEUkN6WcXQVKjdx2CeepC7zRMO4digYFrjnQBrH32mfrlflvihQcg0TXpNvMfnqkEms5DdjtwQzvOOlY3acRBT5SjmorG6O5_2gnjunw',
+  clientID: '24db18fe385c4ddfb9eb77bd70816b21',
+  clientSecret: '49013f8eddb942499b71c3257d6e177b',
 });
 
-const albums = spotifyWrapper.search.albums('Post Malone');
+// spotifyWrapper.auth.authorization()
+  // .then(data => console.log(data));
 
-albums.then(data => data.albums.items.map(item => console.log(item.name)));
+console.log(spotifyWrapper);
+
+// const albums = spotifyWrapper.search.albums('Post Malone');
+
+// albums.then(data => data.albums.items.map(item => console.log(item.name)));

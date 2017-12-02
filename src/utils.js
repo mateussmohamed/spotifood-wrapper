bd074ddfbc0528191ba12aa486890e5e0135e5ca
Mateus Santana <mateuss.dev@gmail.com>
2017-12-02 12:33:40 -0200

    Add parseURL

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,7 @@
-const toJSON = data => data.json();
-export default toJSON;
+export const toJSON = data => data.json();
+
+export const parseURL = (baseURL, params) => {
+  const url = new URL(baseURL); //eslint-disable-line
+  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+  return url.href;
+};

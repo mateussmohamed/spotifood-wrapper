e56b1eb941a4625559d58c0ea0f5e08898562e31
Mateus Santana <mateuss.dev@gmail.com>
2017-12-02 14:06:52 -0200

    Fix babel-polyfill

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,7 +3,7 @@ import { join } from 'path';
 const include = join(__dirname, 'src');
 
 export default {
-  entry: ['babel-polyfill', './index'],
+  entry: './index',
   output: {
     path: join(__dirname, 'dist'),
     libraryTarget: 'umd',

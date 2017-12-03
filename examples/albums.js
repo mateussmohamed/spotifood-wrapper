4d4875e97561d471572f2f2822385e6d0a2eebc9
Mateus Santana <mateuss.dev@gmail.com>
2017-12-03 03:27:25 -0200

    Update examples

--- examples/albums.js
+++ examples/albums.js
@@ -2,16 +2,16 @@ import SpotifoodWrapper from '../src/index';
 
 global.fetch = require('node-fetch');
 
-const SpotifoodWrapper = new SpotifoodWrapper({
+const spotifood = new SpotifoodWrapper({
   clientID: '24db18fe385c4ddfb9eb77bd70816b21',
   clientSecret: '49013f8eddb942499b71c3257d6e177b',
 });
 
-// SpotifoodWrapper.auth.authorization()
-  // .then(data => console.log(data));
+const albums = spotifood.search.albums('Post Malone');
+albums.then(data => data.albums.items.map(item => console.log(item.name)));
 
-console.log(SpotifoodWrapper);
+const filters = { locale: 'pt_BR', country: 'BR' };
 
-// const albums = SpotifoodWrapper.search.albums('Post Malone');
+const playlists = spotifood.browser.featuredPlaylists(filters);
 
-// albums.then(data => data.albums.items.map(item => console.log(item.name)));
+playlists.then(data => console.log(data));

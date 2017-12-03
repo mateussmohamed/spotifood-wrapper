e43c23b673e122f38579ff99532dee3305c40917
Mateus Santana <mateuss.dev@gmail.com>
2017-12-03 03:42:15 -0200

    Fix lint

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import auth from "./auth";
-import album from "./album";
-import browser from "./browser";
-import search from "./search";
-import { request } from "./request";
+import auth from './auth';
+import album from './album';
+import browser from './browser';
+import search from './search';
+import { request } from './request';
 
-import { API_URL, REFRESH_TOKEN_TIME } from "./config";
+import { API_URL, REFRESH_TOKEN_TIME } from './config';
 
 export default function SpotifoodWrapper(options) {
-    this.apiURL = API_URL;
-    this.refreshTokenTime = options.refreshTokenTime || REFRESH_TOKEN_TIME;
-    this.clientID = options.clientID;
-    this.clientSecret = options.clientSecret;
+  this.apiURL = API_URL;
+  this.refreshTokenTime = options.refreshTokenTime || REFRESH_TOKEN_TIME;
+  this.clientID = options.clientID;
+  this.clientSecret = options.clientSecret;
 
-    this.album = album.bind(this)();
-    this.auth = auth.bind(this)();
-    this.browser = browser.bind(this)();
-    this.request = request.bind(this);
-    this.search = search.bind(this)();
+  this.album = album.bind(this)();
+  this.auth = auth.bind(this)();
+  this.browser = browser.bind(this)();
+  this.request = request.bind(this);
+  this.search = search.bind(this)();
 
-    this.auth.authorization();
+  this.auth.authorization();
 }
--- src/request.js
+++ src/request.js
@@ -1,21 +1,21 @@
 /* global fetch */
-import Cookies from "js-cookie";
-import { toJSON } from "./utils";
+import Cookies from 'js-cookie';
+import { toJSON } from './utils';
 
 export function unauthorized(resp) {
-  return resp.error && resp.error.status === 401 ? true : false;
+  return resp.error && resp.error.status === 401;
 }
 
 export function successOrError(res) {
-  return unauthorized(res) ? this.request(url) : res;
+  return unauthorized(res) ? this.request() : res;
 }
 
 export function request(url) {
-  const token = Cookies.get("access_token");
+  const token = Cookies.get('access_token');
   const options = {
     headers: {
-      Authorization: `Bearer ${token}`
-    }
+      Authorization: `Bearer ${token}`,
+    },
   };
   return fetch(url, options)
     .then(toJSON)
